7bf1ec41627352ff7aa692d75d332b4a7e3d8dd4
Florian Mirkes <florian.mirkes@mailbox.org>
2022-04-21 13:08:22 +0200

    Fix quotes and typo

--- snippets/postgres.sh
+++ snippets/postgres.sh
@@ -7,9 +7,9 @@ su - postgres -c 'createuser -P <USER>'
 su - postgres -c 'createdb -O <USER> <DB>'
 
 # create superuser
-CREATE ROLE '<USER>' LOGIN SUPERUSER PASSWORD '<PASSWORD>';
+CREATE ROLE <USER> LOGIN SUPERUSER PASSWORD '<PASSWORD>';
 createdb -h 'localhost' -U '<USER>' --password -O '<USER>' '<DB>'
-psql -h 'localhost' -U '<USER>' --passwod # add '-d postgres' if you haven't created a user db
+psql -h 'localhost' -U '<USER>' --password # add '-d postgres' if you haven't created a user db
 
 # create/restore dumps
 su - postgres -c 'pg_dump <DB>' | tee > '<DUMP>'
