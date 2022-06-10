1d638b3064908f0f2a8f3361f31b2034e8273b41
Florian Mirkes <florian.mirkes@mailbox.org>
2022-06-10 10:04:16 +0200

    Add forwarding/proxy command

--- snippets/ssh.sh
+++ snippets/ssh.sh
@@ -1,5 +1,11 @@
 # connect without hostkey verfication
-ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no <REMOTE_HOST>
 
 # generate eliptic curve key
 ssh-keygen -t ed25519 -C "foo@example.com"
+
+# port forwarding
+ssh -R <REMOTE_PORT>:<LOCAL_ADDRESS>:<LOCAL_PORT> <REMOTE_HOST>
+
+# socks proxy
+ssh -D <PROXY_PORT> -C -N <REMOTE_HOST>
