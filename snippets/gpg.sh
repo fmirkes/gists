9ebe9548e5fcccaa27c831591ef6741808f31509
Florian Mirkes <17073102+fmirkes@users.noreply.github.com>
2023-04-18 12:01:45 +0200

    Add snippets for exporting keys

--- snippets/gpg.sh
+++ snippets/gpg.sh
@@ -1,2 +1,8 @@
-# show gpg key id
-gpg --dry-run --import -v 'foo.gpg'
+# show gpg key id of file
+gpg --dry-run --import -v 'pub.gpg'
+
+# export pub key
+gpg --export -a '<UID>' > 'pub.gpg' 
+
+# export private key
+gpg --export-secret-keys -a '<UID>' > 'key.gpg'
