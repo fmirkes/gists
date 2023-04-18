28bc5dcc4bef432d24e930f3d3f6334c40bc0a44
Florian Mirkes <17073102+fmirkes@users.noreply.github.com>
2023-04-18 13:53:05 +0200

    Add cmds for import and key generation

--- snippets/gpg.sh
+++ snippets/gpg.sh
@@ -1,8 +1,14 @@
 # show gpg key id of file
 gpg --dry-run --import -v 'pub.gpg'
 
+# gen keypair
+gpg --full-generate-key
+
 # export pub key
 gpg --export -a '<UID>' > 'pub.gpg' 
 
 # export private key
 gpg --export-secret-keys -a '<UID>' > 'key.gpg'
+
+# import private or public key
+gpg --import 'key.gpg'
