45cc16601b17e1f8e2267bd847b9f1ca1008ecc4
Florian Mirkes <17073102+fmirkes@users.noreply.github.com>
2023-08-04 09:46:39 +0200

    Fix alt name

--- snippets/openssl-gen-selfsigned-cert.sh
+++ snippets/openssl-gen-selfsigned-cert.sh
@@ -1,1 +1,1 @@
-openssl req -x509 -nodes -days 3650 -newkey rsa:2048 -keyout cert.key -out cert.pub -subj /CN=example.com -addext "subjectAltName=foo.example.com"
+openssl req -x509 -nodes -days 3650 -newkey rsa:2048 -keyout cert.key -out cert.pub -subj /CN=example.com -addext "subjectAltName = DNS:foo.example.com"
