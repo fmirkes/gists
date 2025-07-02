0a94eb8087ff3f76f73f4707296b2117a59cb44d
Florian Mirkes <17073102+fmirkes@users.noreply.github.com>
2025-07-02 16:24:22 +0200

    Download package debs with direct dependencies

--- snippets/apt.sh
+++ snippets/apt.sh
@@ -5,5 +5,8 @@ apt-file list <PACKAGE>
 # extract deb
 ar -x <DEB_FILE>
 
-# download package + dependencies debs
+# download package + direct dependencies debs
+apt-get -o 'Dir::Cache=/home/dev/foobar' install --download-only <PACKAGE>
+
+# download package + rescursive dependencies debs
 apt-get -o 'Dir::Cache=/home/dev/foobar' reinstall --download-only <PACKAGE>
